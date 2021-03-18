b37d3e81563f2a06b3c814e64efa231623ae7167
Tony Mamo <tony.mamo@galileo-insights.com>
2021-03-18 16:03:26 -0400

    wrap with throw catch

--- functions/shout-out-image/shout-out-image.js
+++ functions/shout-out-image/shout-out-image.js
@@ -8,69 +8,78 @@ const script = fs.readFileSync(path.resolve(__dirname, './image.js'), 'utf-8');
 exports.handler = async function (event, ctx, callback) {
   const { queryStringParameters } = event;
 
+  let browser = null;
+
   const username = queryStringParameters.username;
   const userData = await getUserData(username);
 
-  const browser = await playwright.launchChromium();
-  const context = await browser._defaultContext;
-  const page = await context.newPage();
+  try {
+    const browser = await playwright.launchChromium();
+    const context = await browser._defaultContext;
+    const page = await context.newPage();
 
-  page.setViewportSize({
-    width: 1200,
-    height: 630,
-  });
-
-  await page.setContent(`<!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8" />
-      <link rel="preload" href="${
-        userData ? userData.image : ''
-      }" as="image" media="(max-width: 600px)" />
-    </head>
-
-    <body style="margin: 0;padding:0">
-      <div id="app">
-        <div
-          style="
-            display: flex;
-            background-color: #c46200;
-            align-items: center;
-            text-align: center;
-            font-size: 72px;
-            font-weight: 900;
-            line-height: 96px;
-            font-family: 'Whitney SSm A', 'Whitney SSm B', sans-serif;
-            width: 1200px;
-            height: 630px;
-            overflow: hidden;
-          "
-        >
-          Check out my trips on packup!
-        </div>
-      </div>
-    </body>
-  </html>
-  `);
-
-  if (userData) {
-    await page.addScriptTag({
-      content: `
-      window.image = "${userData.image}";
-      window.username = "${userData.username}";
-    `,
+    page.setViewportSize({
+      width: 1200,
+      height: 630,
     });
-    await page.addScriptTag({ content: script });
+
+    await page.setContent(`<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <link rel="preload" href="${
+          userData ? userData.image : ''
+        }" as="image" media="(max-width: 600px)" />
+      </head>
+
+      <body style="margin: 0;padding:0">
+        <div id="app">
+          <div
+            style="
+              display: flex;
+              background-color: #c46200;
+              align-items: center;
+              text-align: center;
+              font-size: 72px;
+              font-weight: 900;
+              line-height: 96px;
+              font-family: 'Whitney SSm A', 'Whitney SSm B', sans-serif;
+              width: 1200px;
+              height: 630px;
+              overflow: hidden;
+            "
+          >
+            Check out my trips on packup!
+          </div>
+        </div>
+      </body>
+    </html>
+    `);
+
+    if (userData) {
+      await page.addScriptTag({
+        content: `
+        window.image = "${userData.image}";
+        window.username = "${userData.username}";
+      `,
+      });
+      await page.addScriptTag({ content: script });
+    }
+
+    const boundingRect = await page.evaluate(() => {
+      const app = document.getElementById('app');
+      const { x, y, width, height } = app.children[0].getBoundingClientRect();
+      return { x, y, width, height };
+    });
+
+      const screenshotBuffer = await page.screenshot({ clip: boundingRect });
+  } catch (error) {
+    throw error;
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
-
-  const boundingRect = await page.evaluate(() => {
-    const app = document.getElementById('app');
-    const { x, y, width, height } = app.children[0].getBoundingClientRect();
-    return { x, y, width, height };
-  });
-
-  const screenshotBuffer = await page.screenshot({ clip: boundingRect });
-  await browser.close();
 
   return {
     isBase64Encoded: true,
