da0770983f0f6f23f0d3cf37d5ccd4426b8d5d81
Tony Mamo <tony.mamo@galileo-insights.com>
2021-03-18 11:42:10 -0400

    update url

--- functions/shout-out-image/get-user-data.js
+++ functions/shout-out-image/get-user-data.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
 
-let url = `https://us-central1-getpackup.cloudfunctions.net/getUserInfo`;
+let url = `https://us-central1-getpackup.cloudfunctions.net/getUserInfo?username=`;
 
 exports.getUserData = async function (username) {
-  const userData = await fetch(`${url}?${username}`)
+  const userData = await fetch(`${url}${username}`)
     .then((res) => res.json())
     .then((json) => {
       return {
